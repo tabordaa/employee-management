f58c4bd430b87961b661fe4c2c4259bade885fb7
Kevin Taborda <elalejitho.2014@gmail.com>
2026-09-02 14:32:55 -0500

    feat: Configuración de React Router y navegación

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
 import './App.css';
 
 function App() {
   return (
     <Layout>
-      <h1>Bienvenido a tin sioq</h1>
-      <p>Aquí irá nuestro dashboard pronto</p>
+      <Routes>
+        <Route path="/" element={<h1>Dashboard Page (Soon) </h1>}></Route>
+        <Route path="/employees" element={<h1>Employee Page (Soon) </h1>}></Route>
+        <Route path="*" element={<h1>404 - Not foun</h1>}></Route>
+      </Routes>
     </Layout>
   )
 }
--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Layout.css';
 
 export const Sidebar = () => {
@@ -6,16 +7,22 @@ export const Sidebar = () => {
         <aside className="sidebar">
             <div className="sidebar-brand">
                 <span className="brand-icon">🏢</span>
-                <h2>Enterprise</h2>
+                <h2>BUK</h2>
             </div>
 
             <nav className="sidebar-nav">
-                <a href="/" className="nav-item">
+                <Link
+                    to="/"
+                    className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}
+                >
                     Dashboard
-                </a>
-                <a href="" className="nav-item active">
-                    Employee List
-                </a>
+                </Link>
+                <Link
+                    to="/employees"
+                    className={`nav-item ${location.pathname === '/employees' ? 'active' : ''}`}
+                >
+                    Employee list
+                </Link>
             </nav>
         </aside>
     );
